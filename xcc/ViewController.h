81f932af5ced21ec5b5f49fdb756f7aaa7fba54e
your name <your@email.com>
2016-01-09 14:51:01 +0800

    Initial Commit

---
+++ xcc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcc
+//
+//  Created by admin on 16/1/9.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xcc/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  xcc
+//
+//  Created by admin on 16/1/9.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"测试机");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
